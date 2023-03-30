ae5163087144b6f070e264e4075905a04c6cc4d4
Erik Barroso <coderik1996@gmail.com>
2023-03-30 20:55:45 -0300

    estudos

---
+++ Iniciando no JS/Função/script.js
@@ -0,0 +1,30 @@
+/*function areaQuadrado(lado) {
+  return lado*lado;
+}
+console.log(areaQuadrado(2));*/
+
+
+/*function pi() {
+  return 3.14;
+}
+let total = 5* pi();
+console.log(pi());*/
+
+/*function imc (peso, altura){
+  const imc = peso / (altura **2);
+  return imc;
+}
+console.log(imc(80, 1.8));*/
+
+/*function corFavorita(cor) {
+  if(cor === 'azul') {
+    return 'Eu gosto de praia';
+  } else if (cor === 'verde'){
+    return 'Eu gosto de trilha';
+  } else {
+    return 'Eu não gosto de nada.k kk'
+  }
+}*/
+
+addEventListener('click', function(){console.log('oi');
+});
--- Iniciando no JS/condicionais/script.js
+++ Iniciando no JS/condicionais/script.js
@@ -1,19 +1,38 @@
 // Verifique se a sua idade é maior do que a de algum parente
 // Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
-console.log('oi');
+ let idade1 = 15, idade2 = 14;
+ /*if (idade1 > idade2) {
+  console.log('É maior');
+ } else {
+  console.log('Não é maior');
+ }*/
+
 // Qual valor é retornado na seguinte expressão?
 var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+/*if(expressao){
+  console.log(expressao);
+}*/
 
 // Verifique se as seguintes variáveis são Truthy ou Falsy
 var nome = 'Andre';
+/*console.log(!!nome);
 var idade = 28;
+console.log(!!idade);
 var possuiDoutorado = false;
+console.log(!!possuiDoutorado);
 var empregoFuturo;
+console.log(!!empregoFuturo);
 var dinheiroNaConta = 0;
+console.log(!!dinheiroNaConta);*/
 
 // Compare o total de habitantes do Brasil com China (valor em milhões)
 var brasil = 207;
 var china = 1340;
+if(china > brasil ){
+  console.log('China tem mais habitantes');
+} else {
+  console.log('China tem menos habitantes')
+}
 
 // O que irá aparecer no console?
 if(('Gato' === 'gato') && (5 > 2)) {
@@ -21,6 +40,7 @@ if(('Gato' === 'gato') && (5 > 2)) {
 } else {
   console.log('Falso');
 }
+//Falso
 
 // O que irá aparecer no console?
 if(('Gato' === 'gato') || (5 > 2)) {
