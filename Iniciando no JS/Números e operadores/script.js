d3f108245135d5b150b7aebeb49dfb286ce3f201
Erik Barroso <coderik1996@gmail.com>
2023-03-30 07:05:11 -0300

    estudos

---
+++ Iniciando no JS/Números e operadores/script.js
@@ -0,0 +1,30 @@
+let idade = 2;
+console.log(idade);
+
+
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+// Crie duas expressões que retornem NaN
+var total = 25;
+var total2 = '2';
+console.log(typeof total * total2);
+
+
+// Somar a string '200' com o número 50 e retornar 250
+let n1 = '200';
+let n2 = 50;
+console.log(+n1 + n2);
+
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+let valor = 5;
+console.log(++valor); 
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2; // NaN (Not a Number)
+console.log(numero/2);
---
+++ Iniciando no JS/Tipos de Dados/script.js
@@ -0,0 +1,25 @@
+console.log(typeof 25);
+
+let nome = 'Erik \'Barroso\' santos' ;
+
+let gols = 10 ;
+let frase1 = `Romário fez ${gols*2} gols`;
+
+console.log(frase1);
+
+
+// Declare uma variável contendo uma string
+let nome2 = 'Erik';
+console.log(typeof nome2);
+// Declare uma variável contendo um número dentro de uma string
+let data = '23';
+// Declare uma variável com a sua idade
+let idade = 26;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let sobrenome = ' Barroso';
+console.log(nome2 + sobrenome); 
+// Coloque a seguinte frase em uma variável: It's time
+let frase = 'It\'s time';
+console.log(frase);
+// Verifique o tipo da variável que contém o seu nome
