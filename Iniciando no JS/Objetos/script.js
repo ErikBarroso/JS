c6ce4b2e0ee0581909908ff9d8bcd0b08a1a964f
Erik Barroso <coderik1996@gmail.com>
2023-04-02 06:55:35 -0300

    objetos
    estudando

--- Iniciando no JS/Objetos/script.js
+++ Iniciando no JS/Objetos/script.js
@@ -134,4 +134,51 @@ function jaVisitei(paisesVisitados) {
 precisoVisitar(20);
 jaVisitei(20);
 
+var menu = {
+  width: 800,
+  height: 50,
+  backgroundColor: '#84E',
+}
+var bg = menu.backgroundColor;
+
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+let dadosPessoais = {
+  nome: 'Erik',
+  sobrenome: 'Barroso',
+  nomeCompleto: function(){
+    return this.nome  + ' ' + this.sobrenome;
+  },
+}
+
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+carro.preco = 3000;
+;
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
+let cachorro = {
+  raca: 'labrador',
+  cor: 'preto',
+  idade: 10,
+  late(pessoa) {
+    if(pessoa === 'homem'){
+    return 'Cachorro latindo';
+    }else{
+      return 'Cachorro não está latindo'
+    }
+  }
+} 
+
+
 
