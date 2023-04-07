c77fa16c1f838dfb2c45519bfaf703ef1ce2492c
Erik Barroso <coderik1996@gmail.com>
2023-04-07 07:12:56 -0300

    number e math

---
+++ Objetos/Number e Math/script.js
@@ -0,0 +1,13 @@
+let valor = 48.49;
+valor = valor.toLocaleString('pt-BR', {style: 'currency',currency: 'BRL'});
+
+console.log(valor);
+
+const aleatorio = Math.floor(Math.random() *10) ;
+console.log(aleatorio);
+
+Math.floor(Math.random() * (72 - 32 + 1)) + 32; 
+Math.floor(Math.random() * (max - min + 1)) + min;
+
+Math.floor(Math.random() * (72 - 32 + 1)) + 32; 
+Math.floor(Math.random() * (max - min + 1)) + min;
