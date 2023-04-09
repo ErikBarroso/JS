ea2226ae03773bb21752de3e5605c68b7fa2dde0
Erik Barroso <coderik1996@gmail.com>
2023-04-08 21:00:01 -0300

    treino

--- Objetos/Number e Math/script.js
+++ Objetos/Number e Math/script.js
@@ -1,13 +1,36 @@
-let valor = 48.49;
-valor = valor.toLocaleString('pt-BR', {style: 'currency',currency: 'BRL'});
 
-console.log(valor);
 
-const aleatorio = Math.floor(Math.random() *10) ;
-console.log(aleatorio);
+//Math.floor(Math.random() * (72 - 32 + 1)) + 32; 
+//Math.floor(Math.random() * (max - min + 1)) + min;
 
-Math.floor(Math.random() * (72 - 32 + 1)) + 32; 
-Math.floor(Math.random() * (max - min + 1)) + min;
+// Retorne um número aleatório
+// entre 1050 e 2000
 
-Math.floor(Math.random() * (72 - 32 + 1)) + 32; 
-Math.floor(Math.random() * (max - min + 1)) + min;
+let aleatorio2 = Math.floor(Math.random() * (2000 - 1050 + 1)) + 1050;
+
+//console.log(aleatorio2);
+
+// Retorne o maior número da lista abaixo
+let numeros = '4, 5, 20, 8, 9';
+const arrayNumeros = numeros.split(', ');
+const numeroMaximo = Math.max(...arrayNumeros);
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+                     'R$ 230  ', 'r$  200'];
+
+
+function limparPreco(preco) {
+  preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
+  preco = +preco.toFixed(2);
+return preco;
+}
+let soma = 0;
+listaPrecos.forEach((preco)=>{
+  soma +=limparPreco(preco);
+})
+console.log(soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+
+limparPreco(listaPrecos[1]);
