726685244a55c2c193f37371d3dc16ab86f1d159
Erik Barroso <coderik1996@gmail.com>
2023-04-13 20:08:30 -0300

    objeto

--- Objetos/Objetos/script.js
+++ Objetos/Objetos/script.js
@@ -14,7 +14,7 @@
 
 const honda = Object.create(carro).init('Honda');
 
-const ferrari = Object.create(carro).init('ferrari'); */
+const ferrari = Object.create(carro).init('ferrari'); 
 
 const moto = {}
 Object.defineProperties(moto, {
@@ -31,3 +31,44 @@ Object.defineProperties(moto, {
 moto.velocidade = 200;
 moto.velocidade;
 // Velocidade 200
+
+*/
+
+// Crie uma função que verifique
+// corretamente o tipo de dado
+
+function verificarDados(dado) {
+  return Object.prototype.toString.call(dado)
+   
+}
+
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+
+const quadrado = {
+ lado: 3,
+}
+Object.defineProperties(quadrado,{
+  lado: {
+    valor: 3,
+    configurable: true,
+    enumarable: false,
+   
+  }
+})
+quadrado.lado = 5
+
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: '#333'
+}
+
+Object.freeze(configuracao); // s
+
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
