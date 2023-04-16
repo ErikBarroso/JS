a6fc9eeabb6d6eedda82b5497c342d4a3ece50cd
Erik Barroso <coderik1996@gmail.com>
2023-04-16 07:16:52 -0300

    fetch
    exercícios consumindo api de CEP

--- JS Assincrono/fetch/script.js
+++ JS Assincrono/fetch/script.js
@@ -7,7 +7,7 @@ doc.then(resolucao => {
 fetch('./doc.txt').then(function(response) {
   console.log(response); // Response HTTP (Objeto)
 });
- */
+ 
 
 fetch('./arquivo.txt').then(function(response) {
   return response.text().then((corpo)=>{
@@ -15,4 +15,50 @@ fetch('./arquivo.txt').then(function(response) {
   conteudo3.innerText = corpo;
 })
 });
+
+const div = document.createElement('div');
+
+fetch('https://viacep.com.br/ws/01001000/json/')
+.then(response => {
+  console.log(response.status, response.ok);
+  if(response.status === 404) {
+    console.log('Página não encontrada')
+  }
+});
+*/
+
+
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// crie um formulário onde o usuário pode digitar o cep
+// e o endereço completo é retornado ao clicar em buscar
+const inputCep = document.getElementById('cep');
+const btnCep = document.getElementById('btncep');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+
+
+btnCep.addEventListener('click', handleClick);
+function handleClick(event){
+  event.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep);
+}
+function buscaCep(cep){
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  .then(response => response.text())
+  .then(body => {
+    resultadoCep.innerText = body;
+  })
+}
+
+
+
+
+// Utilizando a API https://blockchain.info/ticker
+// retorne no DOM o valor de compra da bitcoin and reais.
+// atualize este valor a cada 30s
+
+// Utilizando a API https://api.chucknorris.io/jokes/random
+// retorne uma piada randomica do chucknorris, toda vez que
+// clicar em próxima
 
